dde2e0aa8992f33ff9aa91ac74657ee040b88e50
Lucas Phaelante <lucasdalho@hotmail.com>
2022-10-31 08:08:03 -0300

    teste

--- api/index.js
+++ api/index.js
@@ -7,9 +7,7 @@ const port = process.env.PORT || 3000;
 routes(app)
 
 
-app.get("/", function (req,res){
-    res.send("Olá, estou funcionando")
-})
+
 //fica escutando o servidor
 app.listen(port, () => console.log(`servidor está rodando na porta http://localhost/${port}`))
 
