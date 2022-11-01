fa7a304457c624a4014e1588371bc19f08aa8c5e
Lucas Phaelante <lucasdalho@hotmail.com>
2022-11-01 11:25:29 -0300

    Update Cors

--- api/index.js
+++ api/index.js
@@ -1,9 +1,24 @@
-const express = require('express')
-const routes = require('./routes')
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
 
 const app = express()
 
-const port = 3030;
+
+app.use((req, res, next) => {
+    console.log("MIDDLEWARE ACESSADO"),
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    
+    next();
+});
+	app.use(cors());
+
+const port = 8081;
 routes(app)
 
 
